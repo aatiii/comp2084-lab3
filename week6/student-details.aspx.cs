edc286e0ff86f5677a623662f7a1d6a2b09137a4
aatiii <helen_ati@163.com>
2016-11-03 19:58:51 -0400

    add edit function

--- week6/student-details.aspx.cs
+++ week6/student-details.aspx.cs
@@ -11,11 +11,35 @@ namespace week6
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            if (IsPostBack == false)
+            {
+                // check for adding or deleting
+                if (!String.IsNullOrEmpty(Request.QueryString["StudentID"]))
+                {
+                    // get id
+                    Int32 StudentID = Convert.ToInt32(Request.QueryString["StudentID"]);
+                    // connect
+                    var conn = new ContosoEntities();
+                    // look up the selected student
+                    var objStu = (from s in conn.Students where s.StudentID == StudentID select s).FirstOrDefault();
+                    // populate the form
+                    LastName.Text = objStu.LastName;
+                    FirstMidName.Text = objStu.FirstMidName;
+                    EnrollmentDate.Text = objStu.EnrollmentDate.ToString("yyyy-MM-dd'T'hh:mm:ss");
+                }
+            }
         }
 
         protected void btnSave_Click(object sender, EventArgs e)
         {
+            // check for adding or editing
+            Int32 StudentID = 0;
+
+            if (!String.IsNullOrEmpty(Request.QueryString["StudentID"]))
+            {
+                StudentID = Convert.ToInt32(Request.QueryString["StudentID"]);
+            }
+
             // connect
             var conn = new ContosoEntities();
 
@@ -28,7 +52,17 @@ namespace week6
             s.EnrollmentDate = Convert.ToDateTime(EnrollmentDate.Text);
 
             // save the new object to the database
-            conn.Students.Add(s);
+            if (StudentID == 0)
+            {
+                conn.Students.Add(s);
+            }
+            else
+            {
+                s.StudentID = StudentID;
+                conn.Students.Attach(s);
+                conn.Entry(s).State = System.Data.Entity.EntityState.Modified;
+            }            
+
             conn.SaveChanges();
 
             //redirect to the students page
